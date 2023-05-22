ff158189fc511727b638e33f14bd351518ea1b7b
Anderson <andersonby@163.com>
2023-05-23 00:05:35 +0800

    Use defineModel

--- frontend/src/components/workspace/ListFieldUse.vue
+++ frontend/src/components/workspace/ListFieldUse.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineComponent, defineEmits, watch } from 'vue'
+import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons-vue'
 
@@ -7,39 +7,27 @@ defineComponent({
   name: 'ListFieldUse',
 })
 
-const props = defineProps({
-  value: {
-    type: Array,
-    required: true
-  },
-})
+const listValue = defineModel()
 
 const { t } = useI18n()
 
-const emit = defineEmits(['delete', 'update:value'])
-let innerValue = props.value
 const updateItemValue = (newValue, index) => {
-  innerValue[index] = newValue
-  emit('update:value', innerValue)
+  listValue.value[index] = newValue
 }
 const deleteItem = (index) => {
-  innerValue.splice(index, 1)
-  emit('update:value', innerValue)
+  listValue.value.splice(index, 1)
 }
-watch(() => props.value, (newValue) => {
-  innerValue = newValue
-})
 </script>
 <template>
   <a-row type="flex" :gutter="[12, 12]">
-    <a-col :span="24" :key="index" v-for="(item, index) in innerValue">
+    <a-col :span="24" :key="index" v-for="(item, index) in listValue">
       <div style="display: flex; gap: 5px;">
         <a-input :value="item" @input="updateItemValue($event.target.value, index)" />
         <MinusCircleOutlined @click="deleteItem(index)" />
       </div>
     </a-col>
     <a-col :span="24">
-      <a-button type="dashed" style="width: 100%;" @click="innerValue.push('')">
+      <a-button type="dashed" style="width: 100%;" @click="listValue.push('')">
         <PlusOutlined />
         {{ t('components.nodes.listField.add_item') }}
       </a-button>
