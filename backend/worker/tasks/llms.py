8213041b3cdad1e71baf90b9aa2d1e42d3eecc0d
Anderson <andersonby@163.com>
2023-06-15 00:37:54 +0800

    fix openai endpoint model bug

--- backend/worker/tasks/llms.py
+++ backend/worker/tasks/llms.py
@@ -2,7 +2,7 @@
 # @Author: Bi Ying
 # @Date:   2023-04-26 21:10:52
 # @Last Modified by:   Bi Ying
-# @Last Modified time: 2023-06-03 23:31:09
+# @Last Modified time: 2023-06-15 00:31:10
 from typing import Union
 
 import openai
@@ -10,6 +10,11 @@ import openai
 from utilities.workflow import Workflow
 from utilities.web_crawler import proxies_for_requests
 from worker.tasks import task
+
+
+model_name_map = {
+    "gpt-3.5": "gpt-3.5-turbo",
+}
 
 
 @task
@@ -20,16 +25,17 @@ def open_ai(
     workflow = Workflow(workflow_data)
     input_prompt: Union[str, list] = workflow.get_node_field_value(node_id, "prompt")
     temperature: float = workflow.get_node_field_value(node_id, "temperature")
-    if workflow.setting.get("openai_api_type") == "azure":
+    openai_api_type = workflow.setting.get("openai_api_type")
+    if openai_api_type == "azure":
         openai.api_type = "azure"
         openai.api_base = workflow.setting.get("openai_api_base")
         openai.api_version = "2023-03-15-preview"
-        engine = workflow.setting.get("openai_chat_engine")
+        engin_model_param = {"engine": workflow.setting.get("openai_chat_engine")}
     else:
         openai.api_type = "open_ai"
         openai.api_base = workflow.setting.get("openai_api_base", "https://api.openai.com/v1")
         openai.api_version = None
-        engine = None
+        engin_model_param = {"model": model_name_map.get(workflow.setting.get("llm_model"))}
     openai.api_key = workflow.setting.get("openai_api_key")
     openai.proxy = proxies_for_requests
 
@@ -47,7 +53,7 @@ def open_ai(
             },
         ]
         response = openai.ChatCompletion.create(
-            engine=engine,
+            **engin_model_param,
             messages=messages,
             temperature=temperature,
             max_tokens=2048,
