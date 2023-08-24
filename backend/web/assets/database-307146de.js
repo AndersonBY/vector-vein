943cdb88be41e84ecd35eb933abd309edb13abce
Anderson <andersonby@163.com>
2023-08-24 19:25:22 +0800

    update frontend build

---
+++ backend/web/assets/database-307146de.js
@@ -0,0 +1,1 @@
+import{aH as e}from"./index-3411169e.js";async function n(a,t){return await e(`database__${a}`,t)}async function b(a,t){return await e(`database_object__${a}`,t)}export{b as a,n as d};
--- backend/web/assets/database-e07d11e4.js
+++
@@ -1,1 +0,0 @@
-import{aH as e}from"./index-26c6122c.js";async function n(a,t){return await e(`database__${a}`,t)}async function b(a,t){return await e(`database_object__${a}`,t)}export{b as a,n as d};
