62fbd0c0dc6da14ebfc28ce7c0d7585766582294
Anderson <andersonby@163.com>
2023-08-24 17:58:17 +0800

    handle workflow invoke nodes

--- backend/utilities/workflow.py
+++ backend/utilities/workflow.py
@@ -2,7 +2,8 @@
 # @Author: Bi Ying
 # @Date:   2023-04-13 18:51:34
 # @Last Modified by:   Bi Ying
-# @Last Modified time: 2023-07-23 02:55:48
+# @Last Modified time: 2023-08-24 17:51:49
+import json
 from datetime import datetime
 
 from models import WorkflowRunRecord
@@ -84,6 +85,10 @@ class Node:
         self.__node_data["data"]["credits"] = credits
 
     @property
+    def id(self) -> str:
+        return self.__node_data["id"]
+
+    @property
     def data(self) -> dict:
         return self.__node_data
 
@@ -99,23 +104,156 @@ class Node:
     def category(self) -> str:
         return self.__node_data["category"]
 
+    def __repr__(self) -> str:
+        return f"<Node {self.type} @{self.id}>"
+
 
 class Workflow:
     def __init__(self, workflow_data: dict):
         self.workflow_data = workflow_data
-        self.nodes = {node["id"]: Node(node) for node in workflow_data["nodes"]}
-        self.edges = workflow_data["edges"]
-        self.dag = DAG()
+        if "original_workflow_data" not in workflow_data:
+            self.original_workflow_data = json.loads(json.dumps(workflow_data))
+            self.workflow_data["original_workflow_data"] = self.original_workflow_data
+        else:
+            self.original_workflow_data = workflow_data["original_workflow_data"]
+        self.related_workflows = workflow_data.get("related_workflows", {})
+        self.edges = self.workflow_data["edges"]
+        self.nodes, self.workflow_invoke_nodes = self.parse_nodes()
+        self.dag = self.create_dag()
+        self.workflow_id = workflow_data.get("wid")
+        self.record_id = workflow_data.get("rid")
+
+    def parse_nodes(self):
+        # 在没有【工作流调用】节点的情况下，所有节点就是原始数据中的nodes
+        nodes_list: list[dict] = self.workflow_data["nodes"]
+        nodes: dict[str, Node] = {}
+        workflow_invoke_nodes: dict[Node] = {}
+        while len(nodes_list) > 0:
+            node = nodes_list.pop(0)
+            node_obj = Node(node)
+            if node_obj.type != "WorkflowInvoke":
+                nodes[node["id"]] = node_obj
+                continue
+
+            # 对于【工作流调用】节点，需要将子工作流的节点和边添加到当前工作流中
+            nodes_list.extend(self.handle_workflow_invoke(node_obj))
+            workflow_invoke_nodes[node_obj.id] = node_obj
+
+        # 更新原始数据中的nodes
+        self.workflow_data["nodes"] = [node.data for node in nodes.values()]
+        return nodes, workflow_invoke_nodes
+
+    def handle_workflow_invoke(self, node_obj: Node) -> list[dict]:
+        """
+        对【工作流调用】节点进行处理，将被调用的子工作流的节点和边添加到当前工作流中
+
+        Args:
+            node_obj (Node): 【工作流调用】节点
+
+        Returns:
+            list[dict]: 子工作流的节点列表
+        """
+        related_subnodes = self.get_related_subnodes(node_obj)
+        subworkflow_id = node_obj.get_field("workflow_id").get("value")
+        subworkflow = self.related_workflows.get(subworkflow_id)
+        return self.add_subnodes_and_subedges(subworkflow, related_subnodes, node_obj)
+
+    def get_related_subnodes(self, node_obj: Node) -> list[str]:
+        """
+        找出【工作流调用】节点的显示字段有哪些，并且找出字段对应的实际工作流节点 ID
+
+        Args:
+            node_obj (Node): 【工作流调用】节点
+
+        Returns:
+            list[str]: 关联的子工作流节点 ID 列表
+        """
+        related_subnodes = []
+        for field, field_data in node_obj.field_map.items():
+            if field in ("workflow_id", "fail_all", "list_input"):
+                continue
+            related_subnode = field_data.get("node")
+            if related_subnode is not None:
+                related_subnodes.append(related_subnode)
+        return related_subnodes
+
+    def add_subnodes_and_subedges(
+        self,
+        subworkflow: dict,
+        related_subnodes: list[str],
+        node_obj: Node,
+    ):
+        updated_subnodes = []
+        for subnode in subworkflow.get("nodes", []):
+            updated_subnode = self.add_subnode(subnode, related_subnodes, node_obj)
+            updated_subnodes.append(updated_subnode)
+
+        self.edges.extend(subworkflow.get("edges", []))
+        return updated_subnodes
+
+    def add_subnode(
+        self,
+        subnode: dict,
+        related_subnodes: list[str],
+        node_obj: Node,
+    ):
+        subnode_obj = Node(subnode)
+        # 如果是在【工作流调用】节点中显示的节点，不能直接将原始节点添加到当前工作流中
+        # 需要将原始节点的字段的值更新为【工作流调用】节点显示的字段的值
+        if subnode_obj.id in related_subnodes:
+            self.update_subnode_fields(subnode_obj, node_obj)
+        return subnode_obj.data
+
+    def update_subnode_fields(self, subnode_obj: Node, node_obj: Node):
+        """
+        如果是在【工作流调用】节点中显示的节点，不能直接将原始节点添加到当前工作流中
+        需要将原始节点的字段的值更新为【工作流调用】节点显示的字段的值
+
+        Args:
+            subnode_obj (Node): 【工作流调用】节点中显示的子工作流节点
+            node_obj (Node): 【工作流调用】节点
+        """
+        for subnode_field, subnode_field_data in subnode_obj.field_map.items():
+            if subnode_field in ("workflow_id", "fail_all", "list_input"):
+                continue
+            if subnode_field not in node_obj.field_map:
+                # 不在【工作流调用】节点中显示的字段，不需要更新
+                continue
+            subnode_field_data["value"] = node_obj.get_field(subnode_field)["value"]
+            subnode_obj.update_field(subnode_field, subnode_field_data)
+
+    def create_dag(self):
+        dag = DAG()
         for edge in self.edges:
-            self.dag.add_edge(edge["source"], edge["target"])
-        # add isolated nodes
-        all_nodes = self.dag.get_all_nodes()
+            edge = self.update_edge_nodes(edge)
+            dag.add_edge(edge["source"], edge["target"])
+        self.add_isolated_nodes_to_dag(dag)
+        return dag
+
+    def update_edge_nodes(self, edge: dict):
+        source = edge["source"]
+        target = edge["target"]
+        if source in self.workflow_invoke_nodes:
+            workflow_invoke_node = self.workflow_invoke_nodes[source]
+            original_source_node_field = workflow_invoke_node.get_field(edge["sourceHandle"])
+            original_output_field_key = original_source_node_field.get("output_field_key")
+            source = self.get_original_node(source, edge["sourceHandle"])
+            edge["source"] = source
+            edge["sourceHandle"] = original_output_field_key
+        if target in self.workflow_invoke_nodes:
+            target = self.get_original_node(target, edge["targetHandle"])
+            edge["target"] = target
+        return edge
+
+    def get_original_node(self, node: str, handle: str):
+        return self.workflow_invoke_nodes[node].get_field(handle).get("node")
+
+    def add_isolated_nodes_to_dag(self, dag):
+        all_nodes = dag.get_all_nodes()
         for node_id in self.nodes:
             node = self.get_node(node_id)
             if node_id not in all_nodes and node.category not in ["triggers", "assistedNodes"]:
-                self.dag.add_node(node_id)
-        self.workflow_id = workflow_data["wid"]
-        self.record_id = workflow_data["rid"]
+                dag.add_node(node_id)
 
     def get_sorted_task_order(self) -> list:
         nodes_order = self.dag.topological_sort()
@@ -130,6 +268,47 @@ class Workflow:
                 }
             )
         return tasks
+
+    def get_field_actual_node(self, node: Node, field_data: dict):
+        if node.type != "WorkflowInvoke":
+            return node
+        subworkflow_id = node.get_field("workflow_id").get("value")
+        subworkflow = self.related_workflows.get(subworkflow_id)
+        field_source_node_id = node.get_field(field_data.get("field_key")).get("node")
+        for subnode in subworkflow.get("nodes", []):
+            subnode_obj = Node(subnode)
+            if subnode_obj.id == field_source_node_id:
+                return self.get_field_actual_node(subnode_obj, field_data)
+
+    def update_original_workflow_data(self):
+        """
+        当存在【工作流调用】节点时，我们是将子工作流的节点和边添加到当前工作流中的。
+        但最终返回给用户时不需要展开的工作流节点和边，因此需要替换为原始的节点并更新节点数据。
+        """
+        updated_nodes = []
+        for node_data in self.original_workflow_data["nodes"]:
+            original_node = Node(node_data)
+            used_node = self.get_node(original_node.id)
+            if used_node is not None:
+                updated_nodes.append(used_node.data)
+                continue
+            for field, field_data in original_node.field_map.items():
+                if field in ("workflow_id", "fail_all", "list_input"):
+                    continue
+                actual_node = self.get_field_actual_node(original_node, {"field_key": field, **field_data})
+                actual_field_data = actual_node.get_field(field)
+                if len(actual_field_data) == 0:
+                    actual_field_data = actual_node.get_field(field_data.get("output_field_key"))
+                field_data["value"] = actual_field_data.get("value")
+                original_node.update_field(field, field_data)
+            updated_nodes.append(original_node.data)
+
+        self.workflow_data["nodes"] = updated_nodes
+        self.workflow_data["edges"] = self.original_workflow_data["edges"]
+
+    def clean_workflow_data(self):
+        self.workflow_data.pop("original_workflow_data", None)
+        self.workflow_data.pop("related_workflows", None)
 
     def get_node(self, node_id: str) -> Node:
         return self.nodes.get(node_id)
