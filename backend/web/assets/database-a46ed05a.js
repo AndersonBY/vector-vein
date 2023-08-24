86abe0c13778dc5d907ce69203e9c1d4f2f247eb
Anderson <andersonby@163.com>
2023-08-24 18:00:50 +0800

    update frontend build

---
+++ backend/web/assets/database-a46ed05a.js
@@ -0,0 +1,1 @@
+import{aH as e}from"./index-266077d3.js";async function n(a,t){return await e(`database__${a}`,t)}async function b(a,t){return await e(`database_object__${a}`,t)}export{b as a,n as d};
---
+++ backend/web/assets/userAccount-d2b3608e.js
@@ -0,0 +1,1 @@
+import{a5 as e}from"./index-266077d3.js";const r=()=>{try{return JSON.parse(localStorage.getItem("userAccount")||"{}")}catch{return{}}},o=e("userAccount",{state:()=>({userAccount:r()}),actions:{setUserAccount(t){this.userAccount=t,localStorage.setItem("userAccount",JSON.stringify(this.userAccount))}}});export{o as u};
