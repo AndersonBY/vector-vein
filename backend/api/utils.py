e3d3f9d2aac3a6e2dcf083894b19ef737365199b
Anderson <andersonby@163.com>
2023-08-24 17:56:17 +0800

    add WorkflowData util class

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -2,7 +2,12 @@
 # @Author: Bi Ying
 # @Date:   2023-05-15 14:21:40
 # @Last Modified by:   Bi Ying
-# @Last Modified time: 2023-05-18 13:16:16
+# @Last Modified time: 2023-08-24 16:52:39
+from functools import cached_property
+
+from models import Workflow
+
+
 def get_user_object_general(ObjectClass, **kwargs):
     if len(kwargs) == 0:
         return 500, "wrong args", {}
@@ -11,3 +16,30 @@ def get_user_object_general(ObjectClass, **kwargs):
     except ObjectClass.DoesNotExist:
         return 404, "not exist", {}
     return 200, "", object
+
+
+class WorkflowData:
+    def __init__(self, workflow_data: dict):
+        self.workflow_data = workflow_data
+
+    @cached_property
+    def related_workflows(self) -> dict:
+        related_workflows = {}
+        for node in self.workflow_data["nodes"]:
+            if node["type"] == "WorkflowInvoke":
+                workflow_id = node["data"]["template"]["workflow_id"]["value"]
+                _, _, workflow = get_user_object_general(Workflow, wid=workflow_id)
+                related_workflows.update(workflow.data.get("related_workflows", {}))
+                related_workflows[workflow_id] = workflow.data
+
+        return related_workflows
+
+    def replace_workflow_invoke_nodes_ids(self, id_map: dict):
+        for node in self.workflow_data["nodes"]:
+            if node["type"] != "WorkflowInvoke":
+                continue
+            workflow_id = node["data"]["template"]["workflow_id"]["value"]
+            if workflow_id not in id_map:
+                continue
+            node["data"]["template"]["workflow_id"]["value"] = id_map[workflow_id]
+        return self.workflow_data
