a4687dcb1eba06a09dbd9a2a8134efbd49761d17
Anderson <andersonby@163.com>
2023-09-08 18:34:49 +0800

    update frontend build

--- backend/web/assets/userAccount-eb1b4fb9.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-84468ea1.js";const r=()=>{try{return JSON.parse(localStorage.getItem("userAccount")||"{}")}catch{return{}}},o=e("userAccount",{state:()=>({userAccount:r()}),actions:{setUserAccount(t){this.userAccount=t,localStorage.setItem("userAccount",JSON.stringify(this.userAccount))}}});export{o as u};
