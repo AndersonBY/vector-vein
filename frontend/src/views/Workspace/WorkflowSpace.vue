ebf9e635d8dd882c04977a1c987826f601f360db
Anderson <andersonby@163.com>
2024-05-01 03:29:09 +0800

    update

--- frontend/src/views/Workspace/WorkflowSpace.vue
+++ frontend/src/views/Workspace/WorkflowSpace.vue
@@ -1,12 +1,13 @@
 <script setup>
-import { ref, nextTick, watch } from "vue"
+import { ref, nextTick, watch, onBeforeMount } from "vue"
 import { useI18n } from 'vue-i18n'
-import { storeToRefs } from 'pinia'
 import { useRoute, useRouter } from "vue-router"
 import { message } from 'ant-design-vue'
+import { Home, Star, More, Delete } from '@icon-park/vue-next'
+import { storeToRefs } from 'pinia'
+import { formatTime } from '@/utils/util'
 import { useUserSettingsStore } from '@/stores/userSettings'
 import { useUserWorkflowsStore } from "@/stores/userWorkflows"
-import { Home, User } from '@icon-park/vue-next'
 import { workflowAPI } from "@/api/workflow"
 
 const { t } = useI18n()
@@ -18,19 +19,31 @@ const { userFastAccessWorkflows } = storeToRefs(userWorkflowsStore)
 const route = useRoute()
 const router = useRouter()
 const workflowId = ref(route.params.workflowId)
-watch(route, (route) => {
-  workflowId.value = route.params.workflowId
-  selectedKeys.value = [route.params.workflowId]
-})
-
 const selectedKeys = ref([workflowId.value])
 const openKeys = ref(['user-workflows'])
 
-const newWorkflowIndex = ref(1)
-const newWorkflowModal = ref()
-const openNewWorkflowModal = () => {
-  newWorkflowModal.value.showModal()
+const setSelectedKeys = (route) => {
+  if (route.name == 'WorkflowSpaceMain') {
+    selectedKeys.value = ['my_index']
+  } else if (route.name == 'VAppMain') {
+    selectedKeys.value = ['v_app']
+  } else if (route.name == 'WorkflowUse') {
+    workflowId.value = route.params.workflowId
+    selectedKeys.value = [route.params.workflowId]
+  }
 }
+
+onBeforeMount(async () => {
+  setSelectedKeys(route)
+  await userWorkflowsStore.refreshWorkflows()
+})
+
+watch(route, (route) => {
+  setSelectedKeys(route)
+})
+
+const newWorkflowIndex = ref(1)
+
 const add = async (template) => {
   const response = await workflowAPI('create', {
     title: t('workspace.workflowSpace.new_workflow') + newWorkflowIndex.value,
@@ -41,14 +54,24 @@ const add = async (template) => {
     return
   }
   const workflow = response.data
-  workflow.update_time = new Date(workflow.update_time).toLocaleString()
-  workflow.create_time = new Date(workflow.create_time).toLocaleString()
+  workflow.update_time = formatTime(workflow.update_time)
+  workflow.create_time = formatTime(workflow.create_time)
   userWorkflowsStore.addUserWorkflow(workflow)
 
-  nextTick(() => {
+  nextTick(async () => {
     selectedKeys.value = [workflow.wid]
-    router.push(`/workflow/editor/${workflow.wid}`)
+    await router.push(`/workflow/editor/${workflow.wid}`)
   })
+}
+
+const deleteWorkflowFromFastAccess = async (wid) => {
+  const response = await workflowAPI('delete_from_fast_access', { wid: wid })
+  if (response.status == 200) {
+    message.success(t('workspace.workflowSpace.delete_from_fast_access_success'))
+    await userWorkflowsStore.refreshWorkflows()
+  } else {
+    message.error(t('workspace.workflowSpace.delete_from_fast_access_failed'))
+  }
 }
 </script>
 
@@ -56,40 +79,57 @@ const add = async (template) => {
   <div class="space-container">
     <a-layout class="layout">
       <a-layout-sider width="200" style="background: #fff" breakpoint="lg" collapsed-width="0">
-        <a-skeleton active v-if="loading" />
-        <a-menu v-model:selectedKeys="selectedKeys" v-model:openKeys="openKeys" mode="inline" style="height: 100%"
-          v-else>
-          <a-menu-item key="my_index">
+        <a-skeleton v-if="loading" active />
+        <a-menu v-else v-model:selectedKeys="selectedKeys" v-model:openKeys="openKeys" mode="inline"
+          style="height: 100%">
+          <a-menu-item key="my_index" id="my_index">
             <router-link to="/workflow/">
-              <Home />
+              <Home theme="filled" />
               {{ t('workspace.workflowSpace.workflow_index') }}
             </router-link>
           </a-menu-item>
           <a-sub-menu key="user-workflows">
             <template #title>
               <span>
-                <User />
+                <Star theme="filled" />
                 {{ t('workspace.workflowSpace.user_fast_access_workflows') }}
               </span>
             </template>
             <a-menu-item :key="workflow.wid" v-for="workflow in userFastAccessWorkflows">
-              <a-tooltip placement="topLeft" :title="workflow.title">
-                <router-link :to="`/workflow/${workflow.wid}`">
-                  {{ workflow.title }}
-                </router-link>
-              </a-tooltip>
+              <div class="starred-workflow-item">
+                <a-tooltip placement="topLeft" :title="workflow.title">
+                  <router-link :to="`/workflow/${workflow.wid}`" style="overflow: hidden;">
+                    {{ workflow.title }}
+                  </router-link>
+                </a-tooltip>
+                <a-dropdown>
+                  <div class="starred-workflow-item-more-container">
+                    <div class="single-chat-button-more">
+                      <More />
+                    </div>
+                  </div>
+                  <template #overlay>
+                    <a-menu>
+                      <a-menu-item key="delete" @click=deleteWorkflowFromFastAccess(workflow.wid)>
+                        <a-typography-text type="danger">
+                          <Delete />
+                          {{ t('workspace.workflowSpace.delete_from_fast_access') }}
+                        </a-typography-text>
+                      </a-menu-item>
+                    </a-menu>
+                  </template>
+                </a-dropdown>
+              </div>
             </a-menu-item>
 
             <a-menu-item key="add" @click="add">
               + {{ t('workspace.workflowSpace.add_new_workflow') }}
             </a-menu-item>
-            <!-- <NewWorkflowModal ref="newWorkflowModal" @create="add" /> -->
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        <a-skeleton active v-if="loading" />
-        <router-view :key="workflowId" v-else></router-view>
+        <router-view :key="workflowId"></router-view>
       </a-layout-content>
     </a-layout>
   </div>
@@ -104,5 +144,31 @@ const add = async (template) => {
   height: 100%;
   padding: 24px 0;
   background: #fff;
+}
+
+.space-container .starred-workflow-item {
+  display: flex;
+  justify-content: space-between;
+}
+
+.space-container .starred-workflow-item .starred-workflow-item-more-container {
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 3.5rem;
+  padding: 0 5px;
+  z-index: 5;
+  opacity: 0;
+  background-image: linear-gradient(to left, #e9e9e9 60%, rgba(0, 0, 0, 0));
+  visibility: hidden;
+  transition: opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.space-container .starred-workflow-item:hover .starred-workflow-item-more-container {
+  opacity: 1;
+  visibility: visible;
 }
 </style>
--- frontend/src/views/Workspace/WorkflowTemplate.vue
+++ frontend/src/views/Workspace/WorkflowTemplate.vue
@@ -4,18 +4,26 @@ import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from "vue-router"
 import { useUserWorkflowsStore } from "@/stores/userWorkflows"
 import { message } from 'ant-design-vue'
+import { BranchTwo, DoubleRight, DoubleLeft } from '@icon-park/vue-next'
 import VueMarkdown from 'vue-markdown-render'
+import AuthorComponent from "@/components/AuthorComponent.vue"
+import ImageCarousel from "@/components/ImageCarousel.vue"
+import WorkflowUse from "@/components/workspace/WorkflowUse.vue"
+import RelatedWorkflowsModal from "@/components/workspace/RelatedWorkflowsModal.vue"
 import { officialSiteAPI } from '@/api/remote'
 import { workflowAPI } from '@/api/workflow'
 
 const { t } = useI18n()
 const loading = ref(true)
 const updating = ref(false)
+const addingTemplate = ref(false)
 const route = useRoute()
 const router = useRouter()
 const userWorkflowsStore = useUserWorkflowsStore()
 const workflowTemplateId = route.params.workflowTemplateId
 const templateData = ref({})
+const collapsed = ref(false)
+const showMask = ref(true)
 
 onBeforeMount(async () => {
   const response = await officialSiteAPI('get_template', { tid: workflowTemplateId })
@@ -29,6 +37,7 @@ onBeforeMount(async () => {
 })
 
 const addTemplateToUserWorkflows = async () => {
+  addingTemplate.value = true
   const response = await workflowAPI('create', templateData.value)
   if (response.status == 200) {
     message.success(t('workspace.workflowTemplate.add_success'))
@@ -39,7 +48,8 @@ const addTemplateToUserWorkflows = async () => {
   const workflow = templateData.value
   userWorkflowsStore.addUserWorkflow(workflow)
   userWorkflowsStore.setUserWorkflowsTotal(userWorkflowsStore.userWorkflowsTotal + 1)
-  router.push({ name: 'WorkflowUse', params: { workflowId: response.data.wid } })
+  addingTemplate.value = false
+  await router.push({ name: 'WorkflowUse', params: { workflowId: response.data.wid } })
 }
 </script>
 
@@ -56,105 +66,99 @@ const addTemplateToUserWorkflows = async () => {
       </a-breadcrumb-item>
       <a-breadcrumb-item>{{ templateData.title }}</a-breadcrumb-item>
     </a-breadcrumb>
-    <a-row justify="space-around">
-      <a-col :lg="12" :md="12" :sm="24" :xs="24">
-        <a-typography-title>
-          {{ `${t('workspace.workflowTemplate.template')}: ${templateData.title}` }}
-        </a-typography-title>
-        <div style="margin-bottom: 10px;">
-          <a-space>
-            <a-typography-text type="secondary">
-              {{ t('common.update_time_format', {
-                time: new
-                  Date(parseInt(templateData.update_time)).toLocaleString()
-              }) }}
-            </a-typography-text>
-            <a-divider type="vertical" />
-            <a-tag :color="tag.color" v-for="(tag, index) in templateData.tags" :key="index">
-              {{ tag.title }}
-            </a-tag>
-          </a-space>
-        </div>
-        <div style="margin-bottom: 10px;">
-          <a-space>
-            <a-typography-text type="secondary">
-              {{ t('workspace.workflowTemplate.author', {
-                author: 'VectorVein'
-              }) }}
-            </a-typography-text>
-            <a-divider type="vertical" />
-            <a-typography-text type="secondary" v-if="templateData.used_count > 10">
-              {{ t('workspace.workflowTemplate.used_count', {
-                count: templateData.used_count
-              }) }}
-            </a-typography-text>
-          </a-space>
-        </div>
-        <a-space>
-          <a-button type="primary" @click="addTemplateToUserWorkflows">
-            {{ t('workspace.workflowTemplate.add_to_my_workflows') }}
-          </a-button>
-        </a-space>
-      </a-col>
-    </a-row>
-
-    <a-divider />
-
-    <a-row justify="space-around">
-      <a-col :lg="12" :md="12" :sm="24" :xs="24" v-if="templateData.images.length > 0">
-        <a-carousel autoplay arrows dots-class="slick-dots slick-thumb">
-          <template #customPaging="props">
-            <a>
-              <img :src="templateData.images[props.i]" />
-            </a>
+    <a-row :gutter="[16, 8]" style="margin-top: 30px; padding: 0 10px 30px;">
+      <a-col :xl="collapsed ? 24 : 16" :lg="collapsed ? 24 : 16" :md="24" :xs="24" style="height: 100%;">
+        <a-button type="primary" shape="circle" class="collapse-button" @click="collapsed = !collapsed">
+          <template #icon>
+            <DoubleRight v-if="!collapsed" />
+            <DoubleLeft v-else />
           </template>
-          <div v-for="(image, index) in templateData.images" :key="index">
-            <img :src="image" />
-          </div>
-        </a-carousel>
+        </a-button>
+        <div class="workflow-use-mask" v-if="showMask">
+          <a-button type="primary" @click="showMask = false">
+            {{ t('workspace.workflowTemplate.test_run_template') }}
+          </a-button>
+          <a-typography-text class="hint">
+            {{ t('workspace.workflowTemplate.test_run_template_hint1') }}
+          </a-typography-text>
+        </div>
+        <WorkflowUse :workflow="templateData" :isTemplate="true" />
       </a-col>
-      <a-col :lg="12" :md="12" :sm="24" :xs="24">
-        <VueMarkdown v-highlight :source="templateData.brief" class="custom-scrollbar markdown-body custom-hljs" />
+
+      <a-col :xl="collapsed ? 24 : 8" :lg="collapsed ? 24 : 8" :md="24" :xs="24"
+        style="display: flex; flex-direction: column; gap: 16px;">
+        <a-card>
+          <template #title>
+            <a-space direction="vertical" style="width: 100%; margin-top: 8px; margin-bottom: 8px;">
+              <a-typography-title class="template-title" :level="1" :ellipsis="{ rows: 2 }"
+                :content="templateData.title">
+              </a-typography-title>
+
+              <div style="display: flex; justify-content: space-between; align-items: center;">
+                <AuthorComponent :author="templateData.user" :time="parseInt(templateData.update_time)"
+                  fontColor="#232323" />
+                <RelatedWorkflowsModal :workflowId="workflowTemplateId" :isTemplate="true" />
+              </div>
+            </a-space>
+          </template>
+          <a-space direction="vertical" style="width: 100%; margin-top: 8px; margin-bottom: 8px;">
+            <a-button type="primary" size="large" block @click="addTemplateToUserWorkflows" :loading="addingTemplate"
+              style="margin-bottom: 10px;">
+              {{ t('workspace.workflowTemplate.add_to_my_workflows') }}
+              <a-divider type="vertical" style="border-inline-start-color: rgb(255, 255, 255);" />
+              <BranchTwo />{{ templateData.used_count }}
+            </a-button>
+
+            <a-button v-if="templateData.is_owner" type="primary" @click="openEditor" block>
+              {{ t('workspace.workflowTemplate.edit_template') }}
+            </a-button>
+
+            <ImageCarousel v-if="templateData.images?.length > 0" :images="templateData.images" />
+
+            <VueMarkdown style="width: 100%" v-highlight :source="templateData.brief" :options="{ html: true }"
+              class="custom-scrollbar markdown-body custom-hljs" />
+          </a-space>
+        </a-card>
       </a-col>
+
     </a-row>
   </a-spin>
 </template>
 
 <style scoped>
-:deep(.slick-dots) {
-  position: relative;
-  height: auto;
+.space-container {
+  height: calc(100vh - 64px);
 }
 
-:deep(.slick-slide img) {
-  border: 5px solid #fff;
-  display: block;
-  margin: auto;
-  max-width: 80%;
-  max-height: 60vh;
-}
-
-:deep(.slick-arrow) {
-  display: none !important;
-}
-
-:deep(.slick-thumb) {
-  bottom: 0px;
-}
-
-:deep(.slick-thumb li) {
-  width: 60px;
-  height: 45px;
-}
-
-:deep(.slick-thumb li img) {
+.workflow-use-mask {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
-  filter: grayscale(100%);
-  display: block;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
 }
 
-:deep .slick-thumb li.slick-active img {
-  filter: grayscale(0%);
+.workflow-use-mask .hint {
+  color: #ffffffbd;
+}
+
+.collapse-button {
+  position: absolute;
+  top: 60px;
+  right: -40px;
+  z-index: 10;
+  transform: translate(-50%, 0);
+  box-shadow: 0 0 #0000, 0 0 #0000, 0 4px 6px -1px rgba(0, 0, 0, .2), 0 2px 4px -2px rgba(0, 0, 0, .3);
+}
+
+.template-title {
+  white-space: wrap;
 }
 </style>
