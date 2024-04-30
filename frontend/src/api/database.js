1359846fe136e8db91380a65227f1485397f8c86
Anderson <andersonby@163.com>
2024-05-01 03:17:27 +0800

    add relational database apis

--- frontend/src/api/database.js
+++ frontend/src/api/database.js
@@ -2,7 +2,7 @@
  * @Author: Bi Ying
  * @Date:   2023-02-22 12:26:46
  * @Last Modified by:   Bi Ying
- * @Last Modified time: 2023-07-06 17:23:32
+ * @Last Modified time: 2024-04-30 12:05:49
  */
 import baseAPI from './base'
 
@@ -12,4 +12,16 @@ export async function databaseAPI(action, parameter) {
 
 export async function databaseObjectAPI(action, parameter) {
   return await baseAPI(`database_object__${action}`, parameter)
+}
+
+export async function relationalDatabaseAPI(action, parameter) {
+  return await baseAPI(`relational_database__${action}`, parameter)
+}
+
+export async function relationalDatabaseTableAPI(action, parameter) {
+  return await baseAPI(`relational_database_table__${action}`, parameter)
+}
+
+export async function relationalDatabaseTableRecordAPI(action, parameter) {
+  return await baseAPI(`relational_database_table_record__${action}`, parameter)
 }
