da495a090f7b8054460874a1664061f1c34467aa
Anderson <andersonby@163.com>
2024-05-01 03:27:09 +0800

    update drawer styles

--- frontend/src/components/workspace/WorkflowRunRecordsDrawer.vue
+++ frontend/src/components/workspace/WorkflowRunRecordsDrawer.vue
@@ -2,7 +2,16 @@
 import { ref, reactive, computed } from "vue"
 import { useI18n } from 'vue-i18n'
 import { message } from 'ant-design-vue'
-import { Time, Control, Consume, Tag, ListNumbers } from '@icon-park/vue-next'
+import {
+  Time,
+  Control,
+  Tag,
+  ListNumbers,
+  Login,
+  HourglassFull,
+} from '@icon-park/vue-next'
+import { formatTime } from "@/utils/util"
+import WorkflowRecordsStatusTag from "@/components/workspace/WorkflowRecordStatusTag.vue"
 import { workflowRunRecordAPI } from "@/api/workflow"
 
 const { t } = useI18n()
@@ -33,21 +42,7 @@ const props = defineProps({
 
 const drawerWidth = props.showWorkflowTitle ? '80vw' : '70vw'
 
-const statusColor = {
-  NOT_STARTED: 'purple',
-  QUEUED: 'orange',
-  RUNNING: 'blue',
-  FINISHED: 'green',
-  FAILED: 'red',
-}
-
 const open = ref(false)
-
-const showDrawer = async () => {
-  open.value = true
-  await workflowRunRecords.load({})
-  loading.value = false
-}
 
 const onClose = () => {
   open.value = false
@@ -60,37 +55,51 @@ const statusOptions = [
   { text: t('components.workspace.workflowRunRecordsDrawer.status_running'), value: 'RUNNING' },
   { text: t('components.workspace.workflowRunRecordsDrawer.status_finished'), value: 'FINISHED' },
   { text: t('components.workspace.workflowRunRecordsDrawer.status_failed'), value: 'FAILED' },
+  { text: t('components.workspace.workflowRunRecordsDrawer.status_stopping'), value: 'STOPPING' },
+  { text: t('components.workspace.workflowRunRecordsDrawer.status_stopped'), value: 'STOPPED' },
 ]
-const columns = ref([{
-  title: t('components.workspace.workflowRunRecordsDrawer.start_time'),
-  key: 'start_time',
-  dataIndex: 'start_time',
-  sorter: true,
-  sortDirections: ['descend', 'ascend'],
-  width: '156px',
-}, {
-  title: t('components.workspace.workflowRunRecordsDrawer.end_time'),
-  key: 'end_time',
-  dataIndex: 'end_time',
-  sorter: true,
-  sortDirections: ['descend', 'ascend'],
-  width: '156px',
-}, {
-  title: t('components.workspace.workflowRunRecordsDrawer.status'),
-  key: 'status',
-  dataIndex: 'status',
-  filters: statusOptions,
-  width: '150px',
-}, {
-  title: t('common.action'),
-  key: 'action',
-  width: '100px',
-}])
+const columns = ref([
+  {
+    title: t('components.workspace.workflowRunRecordsDrawer.start_time'),
+    key: 'start_time',
+    dataIndex: 'start_time',
+    sorter: true,
+    sortDirections: ['descend', 'ascend'],
+    width: '156px',
+  },
+  {
+    title: t('components.workspace.workflowRunRecordsDrawer.end_time'),
+    key: 'end_time',
+    dataIndex: 'end_time',
+    sorter: true,
+    sortDirections: ['descend', 'ascend'],
+    width: '156px',
+  },
+  {
+    title: t('components.workspace.workflowRunRecordsDrawer.run_time'),
+    key: 'run_time',
+    dataIndex: 'run_time',
+    width: '100px',
+  },
+  {
+    title: t('components.workspace.workflowRunRecordsDrawer.status'),
+    key: 'status',
+    dataIndex: 'status',
+    filters: statusOptions,
+    width: '100px',
+  },
+  {
+    title: t('common.action'),
+    key: 'action',
+    width: '100px',
+  }
+])
 if (props.showWorkflowTitle) {
   columns.value.splice(0, 0, {
     title: t('components.workspace.workflowRunRecordsDrawer.workflow_title'),
     key: 'workflow_title',
     dataIndex: 'workflow_title',
+    ellipsis: true,
   })
 }
 const workflowRunRecords = reactive({
@@ -99,18 +108,28 @@ const workflowRunRecords = reactive({
   current: 1,
   pageSize: 10,
   total: 0,
+  sort_field: 'start_time',
+  sort_order: 'descend',
+  filters: {
+    status: [],
+  },
   pagination: computed(() => ({
     total: workflowRunRecords.total,
     current: workflowRunRecords.current,
     pageSize: workflowRunRecords.pageSize,
   })),
   handleTableChange: (page, filters, sorter) => {
+    workflowRunRecords.sort_field = sorter.field
+    workflowRunRecords.sort_order = sorter.order
+    workflowRunRecords.filters.status = filters.status
+    workflowRunRecords.filters.shared = filters.shared
     workflowRunRecords.load({
       page_size: page.pageSize,
       page: page.current,
       sort_field: sorter.field,
       sort_order: sorter.order,
       status: filters.status,
+      need_workflow: props.showWorkflowTitle,
     })
   },
   load: async (params) => {
@@ -121,8 +140,13 @@ const workflowRunRecords = reactive({
     })
     if (res.status == 200) {
       workflowRunRecords.data = res.data.records.map(item => {
-        item.start_time = item.start_time ? new Date(item.start_time).toLocaleString() : '-'
-        item.end_time = item.end_time ? new Date(item.end_time).toLocaleString() : '-'
+        if (item.start_time && item.end_time) {
+          item.run_time = ((parseInt(item.end_time) - parseInt(item.start_time)) / 1000).toFixed(2) + 's'
+        } else {
+          item.run_time = '-'
+        }
+        item.start_time = item.start_time ? formatTime(item.start_time) : '-'
+        item.end_time = item.end_time ? formatTime(item.end_time) : '-'
         return item
       })
     } else {
@@ -134,6 +158,20 @@ const workflowRunRecords = reactive({
     workflowRunRecords.loading = false
   }
 })
+
+const showDrawer = async () => {
+  open.value = true
+  await workflowRunRecords.load({
+    page_size: workflowRunRecords.pageSize,
+    page: workflowRunRecords.current,
+    sort_field: workflowRunRecords.sort_field,
+    sort_order: workflowRunRecords.sort_order,
+    status: workflowRunRecords.filters.status,
+    shared: workflowRunRecords.filters.shared,
+    need_workflow: props.showWorkflowTitle,
+  })
+  loading.value = false
+}
 
 const getWorkflowRunRecordDetail = async (rid, workflow) => {
   loading.value = true
@@ -172,27 +210,24 @@ const getWorkflowRunRecordDetail = async (rid, workflow) => {
                 <ListNumbers />
                 {{ t('components.workspace.workflowRunRecordsDrawer.workflow_title') }}
               </template>
-
               <template v-else-if="column.key === 'start_time'">
                 <Time />
                 {{ t('components.workspace.workflowRunRecordsDrawer.start_time') }}
               </template>
-
               <template v-else-if="column.key === 'end_time'">
                 <Time />
                 {{ t('components.workspace.workflowRunRecordsDrawer.end_time') }}
               </template>
-
+              <template v-else-if="column.key === 'run_time'">
+                <a-tooltip :title="t('components.workspace.workflowRunRecordsDrawer.run_time')">
+                  <HourglassFull />
+                </a-tooltip>
+              </template>
               <template v-else-if="column.key === 'status'">
-                <Tag />
-                {{ t('components.workspace.workflowRunRecordsDrawer.status') }}
+                <a-tooltip :title="t('components.workspace.workflowRunRecordsDrawer.status')">
+                  <Tag />
+                </a-tooltip>
               </template>
-
-              <template v-else-if="column.key === 'used_credits'">
-                <Consume />
-                {{ t('components.workspace.workflowRunRecordsDrawer.used_credits') }}
-              </template>
-
               <template v-else-if="column.key === 'action'">
                 <Control />
                 {{ t('common.action') }}
@@ -203,19 +238,19 @@ const getWorkflowRunRecordDetail = async (rid, workflow) => {
               <template v-if="column.key === 'workflow_title'">
                 {{ record.workflow.title }}
               </template>
-
               <template v-else-if="column.key === 'status'">
-                <a-tag :color="statusColor[record.status]">
-                  {{ t(`components.workspace.workflowRunRecordsDrawer.status_${record.status.toLowerCase()}`) }}
-                </a-tag>
+                <WorkflowRecordsStatusTag :status="record.status" />
               </template>
-
               <template v-else-if="column.key === 'action'">
-                <div class="action-container">
-                  <a-typography-link @click.prevent="getWorkflowRunRecordDetail(record.rid, record.workflow)">
-                    {{ t('components.workspace.workflowRunRecordsDrawer.check_record') }}
-                  </a-typography-link>
-                </div>
+                <a-flex gap="small" align="center" justify="center">
+                  <a-tooltip :title="t('components.workspace.workflowRunRecordsDrawer.check_record')">
+                    <a-button type="text" @click.prevent="getWorkflowRunRecordDetail(record.rid, record.workflow)">
+                      <template #icon>
+                        <Login />
+                      </template>
+                    </a-button>
+                  </a-tooltip>
+                </a-flex>
               </template>
             </template>
           </a-table>
