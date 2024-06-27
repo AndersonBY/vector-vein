e1b577b4cab2b38337f58a0dac4753c3068b433f
Anderson <andersonby@163.com>
2024-06-27 19:20:23 +0800

    file name editable

--- frontend/src/components/nodes/fileProcessing/FileUpload.vue
+++ frontend/src/components/nodes/fileProcessing/FileUpload.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 import BaseNode from '@/components/nodes/BaseNode.vue'
+import BaseField from '@/components/nodes/BaseField.vue'
 import { createTemplateData } from './FileUpload'
 
 const props = defineProps({
@@ -14,6 +16,8 @@ const props = defineProps({
   },
 })
 
+const { t } = useI18n()
+
 const fieldsData = ref(props.data.template)
 const templateData = createTemplateData()
 Object.entries(templateData.template).forEach(([key, value]) => {
@@ -22,9 +26,21 @@ Object.entries(templateData.template).forEach(([key, value]) => {
     fieldsData.value[key].is_output = true
   }
 })
+
+const showEditField = ref(false)
 </script>
 
 <template>
   <BaseNode :nodeId="id" :debug="props.data.debug" :fieldsData="fieldsData"
-    translatePrefix="components.nodes.fileProcessing.FileUpload" documentPath="/help/docs/file-processing#h2-4" />
+    translatePrefix="components.nodes.fileProcessing.FileUpload" documentPath="/help/docs/file-processing#h2-4">
+    <template #main>
+      <BaseField :name="fieldsData.files.display_name" required editable @edit="showEditField = true" type="target"
+        v-model:data="fieldsData.files">
+        <a-modal v-model:open="showEditField" :title="t('components.nodes.textProcessing.TextInOut.edit_name')"
+          :footer="null">
+          <a-input v-model:value="fieldsData.files.display_name" />
+        </a-modal>
+      </BaseField>
+    </template>
+  </BaseNode>
 </template>
