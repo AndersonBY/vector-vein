5041db9309149711cac02f32e04cfec32b3ef8a9
Anderson <andersonby@163.com>
2024-06-27 19:24:56 +0800

    fix indent

--- frontend/src/components/InputSearch.vue
+++ frontend/src/components/InputSearch.vue
@@ -7,38 +7,38 @@ const { t } = useI18n()
 const emit = defineEmits(['search', 'clear-search'])
 
 const search = () => {
-	emit('search')
+  emit('search')
 }
 
 const clearSearch = () => {
-	searchText.value = ''
-	emit('clear-search')
+  searchText.value = ''
+  emit('clear-search')
 }
 </script>
 
 <template>
-	<a-input v-model:value="searchText" :placeholder="t('components.inputSearch.input_search_text')" @press-enter="search"
-		class="search-input">
-		<template #prefix>
-			<Search />
-		</template>
-		<template #suffix>
-			<CloseOne class="clear-search-button" theme="filled" @click="clearSearch" style="cursor: pointer;"
-				v-if="searchText.length > 0" />
-		</template>
-	</a-input>
+  <a-input v-model:value="searchText" :placeholder="t('components.inputSearch.input_search_text')" @press-enter="search"
+    class="search-input">
+    <template #prefix>
+      <Search />
+    </template>
+    <template #suffix>
+      <CloseOne class="clear-search-button" theme="filled" @click="clearSearch" style="cursor: pointer;"
+        v-if="searchText.length > 0" />
+    </template>
+  </a-input>
 </template>
 
 <style scoped>
 .search-input {
-	max-width: 250px;
+  max-width: 250px;
 }
 
 .clear-search-button {
-	margin-right: 4px;
+  margin-right: 4px;
 }
 
 .clear-search-button:hover {
-	color: #28c5e5;
+  color: #28c5e5;
 }
 </style>
