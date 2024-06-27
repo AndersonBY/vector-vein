f5ecd95f84588695edca1904b0888198475fd27e
Anderson <andersonby@163.com>
2024-06-27 19:30:09 +0800

    update constants

--- frontend/src/utils/common.js
+++ frontend/src/utils/common.js
@@ -2,29 +2,32 @@
  * @Author: Bi Ying
  * @Date:   2022-07-19 14:45:35
  * @Last Modified by:   Bi Ying
- * @Last Modified time: 2024-06-05 20:48:45
+ * @Last Modified time: 2024-06-18 02:19:01
  */
 import { h, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { TypographyText, Tag, Flex } from 'ant-design-vue'
 import {
-  Helpcenter,
-  CircleFourLine,
-  DocDetail,
-  Picture,
-  Robot,
-  FourArrows,
-  Printer,
-  Data,
-  EditOne,
   Tool,
+  Data,
+  Robot,
+  EditOne,
+  Printer,
+  Picture,
   ClickTap,
-  CoordinateSystem,
+  DocDetail,
+  FourArrows,
+  Helpcenter,
   RadarThree,
+  CircleFourLine,
+  CoordinateSystem,
 } from '@icon-park/vue-next'
+import { storeToRefs } from 'pinia'
 import { useUserSettingsStore } from "@/stores/userSettings"
 
 export const currentTourVersion = 1
 
-export const chatModelOptions = [
+export const nonLocalChatModelOptions = [
   {
     label: "OpenAI",
     value: "OpenAI",
@@ -36,6 +39,92 @@ export const chatModelOptions = [
       {
         label: "gpt-4",
         value: "gpt-4",
+      },
+      {
+        label: "gpt-4o",
+        value: "gpt-4o",
+      },
+    ]
+  },
+  {
+    label: "MiniMax",
+    value: "MiniMax",
+    children: [
+      {
+        label: "abab5.5-chat",
+        value: "abab5.5-chat",
+      },
+      {
+        label: "abab6-chat",
+        value: "abab6-chat",
+      },
+      {
+        label: "abab6.5s-chat",
+        value: "abab6.5s-chat",
+      },
+    ]
+  },
+  {
+    label: "ZhiPuAI",
+    value: "ZhiPuAI",
+    children: [
+      {
+        label: "glm-3-turbo",
+        value: "glm-3-turbo",
+      },
+      {
+        label: "glm-4",
+        value: "glm-4",
+      },
+      {
+        label: "glm-4-0520",
+        value: "glm-4-0520",
+      },
+      {
+        label: "glm-4-air",
+        value: "glm-4-air",
+      },
+      {
+        label: "glm-4-airx",
+        value: "glm-4-airx",
+      },
+      {
+        label: "glm-4-flash",
+        value: "glm-4-flash",
+      },
+      {
+        label: "glm-4v",
+        value: "glm-4v",
+      },
+    ]
+  },
+  {
+    label: "Qwen",
+    value: "Qwen",
+    children: [
+      {
+        label: "qwen1.5-7b-chat",
+        value: "qwen1.5-7b-chat",
+      },
+      {
+        label: "qwen1.5-14b-chat",
+        value: "qwen1.5-14b-chat",
+      },
+      {
+        label: "qwen1.5-32b-chat",
+        value: "qwen1.5-32b-chat",
+      },
+      {
+        label: "qwen1.5-72b-chat",
+        value: "qwen1.5-72b-chat",
+      },
+      {
+        label: "qwen1.5-110b-chat",
+        value: "qwen1.5-110b-chat",
+      },
+      {
+        label: "qwen2-72b-instruct",
+        value: "qwen2-72b-instruct",
       },
     ]
   },
@@ -58,20 +147,6 @@ export const chatModelOptions = [
     ]
   },
   {
-    label: "ZhiPuAI",
-    value: "ZhiPuAI",
-    children: [
-      {
-        label: "glm-3-turbo",
-        value: "glm-3-turbo",
-      },
-      {
-        label: "glm-4",
-        value: "glm-4",
-      },
-    ]
-  },
-  {
     label: "Anthropic",
     value: "Anthropic",
     children: [
@@ -89,27 +164,132 @@ export const chatModelOptions = [
       },
     ]
   },
+  {
+    label: "Mistral",
+    value: "Mistral",
+    children: [
+      {
+        label: "mixtral-8x22b",
+        value: "mixtral-8x22b",
+      },
+      {
+        label: "mistral-small",
+        value: "mistral-small",
+      },
+      {
+        label: "mistral-medium",
+        value: "mistral-medium",
+      },
+      {
+        label: "mistral-large",
+        value: "mistral-large",
+      },
+    ]
+  },
+  {
+    label: "DeepSeek",
+    value: "DeepSeek",
+    children: [
+      {
+        label: "deepseek-chat",
+        value: "deepseek-chat",
+      },
+      {
+        label: "deepseek-coder",
+        value: "deepseek-coder",
+      },
+    ]
+  },
+  {
+    label: "Yi",
+    value: "Yi",
+    children: [
+      {
+        label: "yi-large",
+        value: "yi-large",
+      },
+      {
+        label: "yi-large-turbo",
+        value: "yi-large-turbo",
+      },
+      {
+        label: "yi-medium",
+        value: "yi-medium",
+      },
+      {
+        label: "yi-medium-200k",
+        value: "yi-medium-200k",
+      },
+      {
+        label: "yi-spark",
+        value: "yi-spark",
+      },
+      // {
+      //   label: "yi-vision",
+      //   value: "yi-vision",
+      // },
+    ]
+  },
+  {
+    label: "Groq",
+    value: "Groq",
+    children: [
+      {
+        label: "mixtral-8x7b-32768",
+        value: "mixtral-8x7b-32768",
+      },
+      {
+        label: "llama3-70b-8192",
+        value: "llama3-70b-8192",
+      },
+      {
+        label: "llama3-8b-8192",
+        value: "llama3-8b-8192",
+      },
+      {
+        label: "gemma-7b-it",
+        value: "gemma-7b-it",
+      },
+    ]
+  },
+  {
+    label: "Gemini",
+    value: "Gemini",
+    children: [
+      {
+        label: "gemini-1.5-pro",
+        value: "gemini-1.5-pro",
+      },
+      {
+        label: "gemini-1.5-flash",
+        value: "gemini-1.5-flash",
+      },
+    ]
+  },
 ]
 
-const flattenModelOptions = (options, showProvider = true) => {
-  const flattenedOptions = [];
-
-  options.forEach(option => {
-    if (option.children && option.children.length > 0) {
-      option.children.forEach(child => {
-        flattenedOptions.push({
-          label: showProvider ? `${option.label}/${child.label}` : child.label,
-          value: showProvider ? `${option.value}/${child.value}` : child.value,
-        });
-      });
+export const chatModelOptions = computed(() => {
+  const { t } = useI18n()
+  const userSettings = useUserSettingsStore()
+  const { setting } = storeToRefs(userSettings)
+  return nonLocalChatModelOptions.concat(setting.value.data?.local_llms?.map((provider) => {
+    return {
+      value: '_local__' + provider.model_family,
+      label: h(TypographyText, {}, () =>
+        h(Flex, { gap: 'small', style: 'display: inline-flex;' }, () => [
+          provider.model_family,
+          h(Tag, { color: 'green', bordered: false }, () => t('workspace.chatSpace.local_model'))
+        ])
+      ),
+      children: provider.models.map((model) => ({
+        value: model.model_id,
+        label: model.model_label,
+      })),
     }
-  });
+  }))
+})
 
-  return flattenedOptions;
-}
-
-export const flattenedChatModelOptions = flattenModelOptions(chatModelOptions)
-
+export const allModels = computed(() => chatModelOptions.value.map((item) => item.children).flat())
 
 export const statusColorMap = {
   'INVALID': 'red',
@@ -210,4 +390,170 @@ export const databaseColumnTypes = [
 
 export const websiteBase = computed(() => {
   return 'https://' + (useUserSettingsStore().setting.data.website_domain ?? 'vectorvein.ai')
+})
+
+export const defaultSettings = {
+  'en-US': {
+    system_prompt: 'You are an AI assistant from VectorVein(Chinese name: 向量脉络) and you can use automated workflows to do all kinds of tasks.',
+    auto_run_workflow: false,
+    opening_dialog: {
+      text: 'Hello! How can I help you?',
+      questions: [],
+    }
+  },
+  'zh-CN': {
+    system_prompt: '你是来自向量脉络的AI助手，你可以使用自动化工作流来完成各种任务。',
+    auto_run_workflow: false,
+    opening_dialog: {
+      text: '您好！有什么可以帮助您的？',
+      questions: [],
+    }
+  }
+}
+
+export const agentVoiceOptions = computed(() => {
+  const { t, te } = useI18n()
+  const options = [
+    {
+      value: 'openai',
+      label: 'openai',
+      children: [
+        {
+          "value": "alloy",
+          "label": "alloy",
+        },
+        {
+          "value": "echo",
+          "label": "echo",
+        },
+        {
+          "value": "fable",
+          "label": "fable",
+        },
+        {
+          "value": "onyx",
+          "label": "onyx",
+        },
+        {
+          "value": "nova",
+          "label": "nova",
+        },
+        {
+          "value": "shimmer",
+          "label": "shimmer",
+        },
+      ]
+    },
+    {
+      value: 'minimax',
+      label: 'minimax',
+      children: [
+        {
+          "value": "male-qn-qingse",
+          "label": "male-qn-qingse",
+        },
+        {
+          "value": "male-qn-jingying",
+          "label": "male-qn-jingying",
+        },
+        {
+          "value": "male-qn-badao",
+          "label": "male-qn-badao",
+        },
+        {
+          "value": "male-qn-daxuesheng",
+          "label": "male-qn-daxuesheng",
+        },
+        {
+          "value": "female-shaonv",
+          "label": "female-shaonv",
+        },
+        {
+          "value": "female-yujie",
+          "label": "female-yujie",
+        },
+        {
+          "value": "female-chengshu",
+          "label": "female-chengshu",
+        },
+        {
+          "value": "female-tianmei",
+          "label": "female-tianmei",
+        },
+        {
+          "value": "presenter_male",
+          "label": "presenter_male",
+        },
+        {
+          "value": "presenter_female",
+          "label": "presenter_female",
+        },
+        {
+          "value": "audiobook_male_1",
+          "label": "audiobook_male_1",
+        },
+        {
+          "value": "audiobook_male_2",
+          "label": "audiobook_male_2",
+        },
+        {
+          "value": "audiobook_female_1",
+          "label": "audiobook_female_1",
+        },
+        {
+          "value": "audiobook_female_2",
+          "label": "audiobook_female_2",
+        },
+        {
+          "value": "male-qn-qingse-jingpin",
+          "label": "male-qn-qingse-jingpin",
+        },
+        {
+          "value": "male-qn-jingying-jingpin",
+          "label": "male-qn-jingying-jingpin",
+        },
+        {
+          "value": "male-qn-badao-jingpin",
+          "label": "male-qn-badao-jingpin",
+        },
+        {
+          "value": "male-qn-daxuesheng-jingpin",
+          "label": "male-qn-daxuesheng-jingpin",
+        },
+        {
+          "value": "female-shaonv-jingpin",
+          "label": "female-shaonv-jingpin",
+        },
+        {
+          "value": "female-yujie-jingpin",
+          "label": "female-yujie-jingpin",
+        },
+        {
+          "value": "female-chengshu-jingpin",
+          "label": "female-chengshu-jingpin",
+        },
+        {
+          "value": "female-tianmei-jingpin",
+          "label": "female-tianmei-jingpin",
+        },
+      ]
+    },
+    {
+      value: 'piper',
+      label: 'piper',
+      children: [
+        {
+          value: 'default',
+          label: 'default',
+        }
+      ]
+    }
+  ]
+  options.forEach((provider) => {
+    provider.children.forEach((voice) => {
+      if (te(`voiceOptions.${provider.value}_${voice.value}`) === false) return
+      voice.label = t(`voiceOptions.${provider.value}_${voice.value}`)
+    })
+  })
+  return options
 })
