dd416870bf1387981e2800d6a458e370487e5f6d
Anderson <andersonby@163.com>
2024-06-27 19:34:01 +0800

    add APIs

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,7 +2,7 @@
  * @Author: Bi Ying
  * @Date:   2022-05-25 01:29:38
  * @Last Modified by:   Bi Ying
- * @Last Modified time: 2023-07-06 17:23:56
+ * @Last Modified time: 2024-06-12 02:12:45
  */
 import baseAPI from './base'
 
@@ -12,4 +12,12 @@ export async function userInfoAPI(action, parameter) {
 
 export async function settingAPI(action, parameter) {
     return await baseAPI(`setting__${action}`, parameter)
+}
+
+export async function hardwareAPI(action, parameter) {
+    return await baseAPI(`hardware__${action}`, parameter)
+}
+
+export async function shortcutAPI(action, parameter) {
+    return await baseAPI(`shortcut__${action}`, parameter)
 }
