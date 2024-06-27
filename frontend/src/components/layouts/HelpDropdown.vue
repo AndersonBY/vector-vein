138e0e0687d5ffd611d890fc30176fd5762366d1
Anderson <andersonby@163.com>
2024-06-27 19:22:36 +0800

    add tag for new update

--- frontend/src/components/layouts/HelpDropdown.vue
+++ frontend/src/components/layouts/HelpDropdown.vue
@@ -5,6 +5,7 @@ import { useI18n } from 'vue-i18n'
 import { storeToRefs } from 'pinia'
 import VueMarkdown from 'vue-markdown-render'
 import { useUserSettingsStore } from '@/stores/userSettings'
+import { websiteBase } from '@/utils/common'
 import { officialSiteAPI } from '@/api/remote'
 
 const { t } = useI18n()
@@ -18,15 +19,14 @@ const updateInfo = reactive({
   release_notes: '',
 })
 
-onBeforeMount(() => {
-  officialSiteAPI('get_update_info', {}).then(res => {
-    if (res.status === 200) {
-      updateInfo.updatable = res.data.updatable
-      updateInfo.version = res.data.version
-      updateInfo.release_date = res.data.release_date
-      updateInfo.release_notes = res.data.release_notes
-    }
-  })
+onBeforeMount(async () => {
+  const res = await officialSiteAPI('get_update_info', {})
+  if (res.status === 200) {
+    updateInfo.updatable = res.data.updatable
+    updateInfo.version = res.data.version
+    updateInfo.release_date = res.data.release_date
+    updateInfo.release_notes = res.data.release_notes
+  }
 })
 
 const openAboutVectorVein = ref(false)
@@ -34,20 +34,30 @@ const openUpdateInfo = ref(false)
 </script>
 
 <template>
-  <a-sub-menu key="/help" :title="t('components.layout.helpDropdown.help')">
+  <a-sub-menu key="/help">
     <template #icon>
       <Help />
     </template>
+    <template #title>
+      {{ t('components.layout.helpDropdown.help') }}
+      <a-tag v-if="updateInfo.updatable" color="green" :bordered="false">New</a-tag>
+    </template>
     <a-menu-item key="document">
-      <a href="https://vectorvein.com/help/docs/introduction" target="_blank">
+      <a :href="`${websiteBase}/help/docs/introduction`" target="_blank">
         {{ t('components.layout.helpDropdown.documentation') }}
       </a>
     </a-menu-item>
     <a-menu-item key="/update" v-if="updateInfo.updatable" @click="openUpdateInfo = true">
       {{ t('components.layout.helpDropdown.update_available') }}
-      <a-modal
-        :title="t('components.layout.helpDropdown.new_version', { version: updateInfo.version, releaseDatetime: new Date(updateInfo.release_date).toLocaleString() })"
-        :open="openUpdateInfo" :footer="null" @cancel="openUpdateInfo = false">
+      <a-modal :open="openUpdateInfo" :footer="null" @cancel="openUpdateInfo = false">
+        <template #title>
+          <a href="https://github.com/AndersonBY/vector-vein/releases">
+            {{ t('components.layout.helpDropdown.new_version', {
+              version: updateInfo.version, releaseDatetime: new
+                Date(updateInfo.release_date).toLocaleString()
+            }) }}
+          </a>
+        </template>
         <VueMarkdown v-highlight :source="updateInfo.release_notes[language]"
           class="custom-scrollbar markdown-body custom-hljs" />
       </a-modal>
