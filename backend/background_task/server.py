3e51bd375bf80fdfcceeedd38f9ea57715652ff0
Anderson <andersonby@163.com>
2024-06-27 18:59:01 +0800

    add background tasks server

---
+++ backend/background_task/server.py
@@ -0,0 +1,110 @@
+# @Author: Bi Ying
+# @Date:   2024-06-06 15:54:30
+# This is a simple simulation of a background task handler like Celery.
+import time
+import traceback
+import multiprocessing
+from pathlib import Path
+
+from diskcache import Deque
+from qdrant_client import QdrantClient
+
+from utilities.general import mprint
+from utilities.config import config, cache
+
+
+class BackgroundTaskServer:
+    def __init__(self, cache_dir: str | Path | None = None, num_workers: int = 2):
+        if cache_dir is None:
+            cache_dir = Path(config.data_path) / "cache"
+        self.cache_dir = Path(cache_dir)
+        self.task_queue_directory = self.cache_dir / "background_task"
+        self.qdrant_tasks_queue_directory = self.cache_dir / "qdrant_task"
+        self.num_workers = num_workers
+        self.processes = []
+
+    def start(self):
+        for worker_num in range(self.num_workers):
+            process = multiprocessing.Process(
+                target=self.run,
+                args=(self.task_queue_directory, worker_num),
+            )
+            process.start()
+            self.processes.append(process)
+
+        # Qdrant local mode can only have one client.
+        # All qdrant tasks should be processed in the same process.
+        # https://github.com/qdrant/qdrant-client
+        qdrant_process = multiprocessing.Process(
+            target=self.run_qdrant_task_server,
+            args=(self.qdrant_tasks_queue_directory,),
+        )
+        qdrant_process.start()
+        self.processes.append(qdrant_process)
+
+    def stop(self):
+        mprint("Stopping background task server...")
+        for process in self.processes:
+            if process:
+                process.terminate()
+                process.join()
+        self.processes = []
+
+    @staticmethod
+    def run(task_queue_direrctory: str | Path | None = None, worker_num: int = 0):
+        from background_task.tasks import get_task
+
+        task_queue = Deque(directory=task_queue_direrctory)
+        sleep_time = 1
+        task_name = ""
+
+        mprint(f"Background task server {worker_num} started.")
+        while True:
+            try:
+                if len(task_queue) > 0:
+                    task = task_queue.pop()
+                    task_name, task_id, args, kwargs = task["task_name"], task["task_id"], task["args"], task["kwargs"]
+                    task_func = get_task(task_name)
+                    if not task_func:
+                        continue
+                    task_result = task_func(*args, **kwargs)
+                    cache.set(f"task_result_{task_id}", task_result, expire=60 * 10)
+                    mprint(f"Task {task_id} {task_name} completed in worker {worker_num}.")
+                    sleep_time = 0.01
+                else:
+                    sleep_time = 1
+            except Exception as e:
+                mprint.error(traceback.format_exc())
+                mprint.error(f"Error running task {task_name} in worker {worker_num}: {e}")
+
+            time.sleep(sleep_time)
+
+    @staticmethod
+    def run_qdrant_task_server(qdrant_tasks_queue_directory: str | Path | None = None):
+        from background_task.tasks import get_task
+
+        qdrant_path = Path(config.data_path) / "qdrant_db"
+        qdrant_client = QdrantClient(path=qdrant_path.absolute())
+        qdrant_tasks_queue = Deque(directory=qdrant_tasks_queue_directory)
+        sleep_time = 1
+
+        mprint("Qdrant task server started.")
+        while True:
+            try:
+                if len(qdrant_tasks_queue) > 0:
+                    task = qdrant_tasks_queue.pop()
+                    task_name, task_id, args, kwargs = task["task_name"], task["task_id"], task["args"], task["kwargs"]
+                    task_func = get_task(task_name)
+                    if not task_func:
+                        continue
+                    task_result = task_func(qdrant_client, *args, **kwargs)
+                    cache.set(f"task_result_{task_id}", task_result, expire=60 * 10)
+                    mprint(f"Task {task_id} {task_name} completed.")
+                    sleep_time = 0.01
+                else:
+                    sleep_time = 1
+            except Exception as e:
+                mprint.error(traceback.format_exc())
+                mprint.error(f"Error running task {task_name}: {e}")
+
+            time.sleep(sleep_time)
---
+++ backend/background_task/tasks.py
@@ -0,0 +1,216 @@
+# @Author: Bi Ying
+# @Date:   2024-06-06 16:04:26
+import uuid
+from pathlib import Path
+
+from diskcache import Deque
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import (
+    Filter,
+    Distance,
+    MatchValue,
+    PointStruct,
+    VectorParams,
+    FilterSelector,
+    FieldCondition,
+)
+
+from models import Workflow, WorkflowTemplate, UserObject
+from utilities.general import mprint
+from utilities.config import config, cache
+from utilities.ai_utils import (
+    ToolCallData,
+    EmbeddingClient,
+    format_messages,
+    conversation_title_generator,
+)
+
+tasks_registry = {}
+
+tasks_queue = Deque(directory=Path(config.data_path) / "cache" / "background_task")
+qdrant_tasks_queue = Deque(directory=Path(config.data_path) / "cache" / "qdrant_task")
+
+
+def background_task(func):
+    tasks_registry[func.__name__] = func
+
+    def wrapper(*args, **kwargs):
+        task_id = uuid.uuid4().hex
+        task = {"task_name": func.__name__, "task_id": task_id, "args": args, "kwargs": kwargs}
+        if is_qdrant_task(func.__name__):
+            qdrant_tasks_queue.appendleft(task)
+        else:
+            tasks_queue.appendleft(task)
+        mprint(f"Task {task_id} {func.__name__} added to queue.")
+        return task["task_id"]
+
+    wrapper.delay = wrapper  # Alias for .delay()
+    return wrapper
+
+
+def get_task(task_name):
+    return tasks_registry.get(task_name)
+
+
+def is_qdrant_task(task_name):
+    return task_name.startswith("q_")
+
+
+@background_task
+def update_workflow_tool_call_data(
+    workflow_wid: str | None = None,
+    template_tid: str | None = None,
+    force: bool = False,
+):
+    if workflow_wid:
+        workflow = Workflow.get(Workflow.wid == workflow_wid)
+    elif template_tid:
+        workflow = WorkflowTemplate.get(WorkflowTemplate.tid == template_tid)
+    else:
+        return False
+
+    tool_call_data = ToolCallData(workflow)
+    tool_call_data.update_title(force=force)
+    tool_call_data.update_parameters()
+    tool_call_data.save()
+
+
+@background_task
+def summarize_conversation_title(
+    message_id: str,
+    messasges: list,
+    backend: str = "OpenAI",
+    model: str = "gpt-35-turbo",
+):
+    formatted_messages = format_messages(messages=messasges, backend=backend)
+    conversation_title = conversation_title_generator(formatted_messages, backend=backend, model=model)[:25]
+    cache.set(f"conversation-title:{message_id}", conversation_title, expire=60 * 60)
+
+
+@background_task
+def q_create_collection(client: QdrantClient, vid: str, size: int = 768):
+    try:
+        client.recreate_collection(
+            collection_name=f"{vid}_text_collection",
+            vectors_config=VectorParams(size=size, distance=Distance.COSINE),
+            on_disk_payload=True,
+        )
+        return True
+    except Exception as e:
+        mprint.error(e)
+        return False
+
+
+@background_task
+def q_delete_collection(client: QdrantClient, vid: str):
+    try:
+        client.delete_collection(f"{vid}_text_collection")
+        return True
+    except Exception as e:
+        mprint.error(e)
+        return False
+
+
+@background_task
+def q_add_point(client: QdrantClient, vid: str, point: dict):
+    try:
+        client.upsert(
+            collection_name=f"{vid}_text_collection",
+            points=[
+                PointStruct(
+                    id=uuid.uuid4().hex,
+                    payload={
+                        "object_id": point.get("object_id"),
+                        "text": point.get("text"),
+                        "embedding_type": point.get("embedding_type"),
+                        "extra_data": point.get("extra_data"),
+                    },
+                    vector=point.get("embedding"),
+                ),
+            ],
+        )
+        if point.get("chunk_index") == point.get("chunk_count") - 1:
+            user_object: UserObject = UserObject.get(UserObject.oid == point.get("object_id"))
+            user_object.status = "VA"
+            user_object.save()
+        cache.set(
+            f"qdrant-point-progress:{vid}:{point.get('object_id')}",
+            {"chunk_index": point.get("chunk_index"), "chunk_count": point.get("chunk_count")},
+            expire=60 * 60,
+        )
+        return True
+    except Exception as e:
+        mprint.error(e)
+        return False
+
+
+@background_task
+def embedding_and_upload(
+    vid: str,
+    object_id: str,
+    input: str | list,
+    embedding_provider: str,
+    embedding_model: str,
+    embedding_type: str,
+    embedding_dimensions: int | None = None,
+    extra_data: dict | None = None,
+):
+    input = input if isinstance(input, list) else [input]
+    if extra_data is None:
+        extra_data = {}
+
+    embedding_client = EmbeddingClient(
+        provider=embedding_provider, model_id=embedding_model, dimensions=embedding_dimensions
+    )
+    for index, text in enumerate(input):
+        embedding = embedding_client.get(text)
+        q_add_point.delay(
+            vid=vid,
+            point={
+                "object_id": object_id,
+                "text": text,
+                "embedding": embedding,
+                "embedding_type": embedding_type,
+                "extra_data": extra_data,
+                "chunk_index": index,
+                "chunk_count": len(input),
+            },
+        )
+    return True
+
+
+@background_task
+def q_delete_point(client: QdrantClient, vid: str, object_id: str):
+    try:
+        client.delete(
+            collection_name=f"{vid}_text_collection",
+            points_selector=FilterSelector(
+                filter=Filter(
+                    must=[
+                        FieldCondition(
+                            key="object_id",
+                            match=MatchValue(value=object_id),
+                        ),
+                    ],
+                )
+            ),
+        )
+        return True
+    except Exception as e:
+        mprint.error(e)
+        return False
+
+
+@background_task
+def q_search_point(
+    client: QdrantClient,
+    vid: str,
+    text_embedding: list,
+    limit: int = 5,
+):
+    text_hits = client.search(
+        collection_name=f"{vid}_text_collection",
+        query_vector=text_embedding,
+        limit=limit,
+    )
+    return [hit.payload for hit in text_hits]
