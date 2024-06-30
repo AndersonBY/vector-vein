9cd33df150d655723a478d2dd8df0ab89aab5e4d
Anderson <andersonby@163.com>
2024-06-30 18:09:24 +0800

    change default user name

--- frontend/src/components/workspace/chat/ChatAuthor.vue
+++ frontend/src/components/workspace/chat/ChatAuthor.vue
@@ -37,7 +37,7 @@ const authorNickname = computed(() => {
   if (props.author.nickname) {
     return props.author.nickname
   } else {
-    return t('common.vectorvein_user')
+    return t('common.me')
   }
 })
 
