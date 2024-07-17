978883723df9b495474e587254230f914fa5dbb8
Anderson <andersonby@163.com>
2024-07-17 15:59:31 +0800

    chore: Update zhipuai models RPM

--- backend/worker/tasks/llms/chat_glm.py
+++ backend/worker/tasks/llms/chat_glm.py
@@ -4,12 +4,21 @@ from .base_llm import BaseLLMTask
 from .types.model import ModelSetting, EndpointSetting
 
 
-endpoints = [
+endpoints_20_rpm = [
     EndpointSetting(
         endpoint_settings_key="zhipuai_api_base",
         api_key_settings_key="zhipuai_api_key",
         rpm=20,
-        tpm=200000,
+        tpm=2000000,
+    )
+]
+
+endpoints_100_rpm = [
+    EndpointSetting(
+        endpoint_settings_key="zhipuai_api_base",
+        api_key_settings_key="zhipuai_api_key",
+        rpm=100,
+        tpm=2000000,
     )
 ]
 
@@ -20,32 +29,37 @@ class ChatGLMTask(BaseLLMTask):
     MODEL_SETTINGS: dict[str, ModelSetting] = {
         "glm-3-turbo": ModelSetting(
             id="glm-3-turbo",
-            endpoints=endpoints,
-            max_output_tokens=8192,
+            endpoints=endpoints_100_rpm,
+            max_output_tokens=4095,
         ),
         "glm-4": ModelSetting(
             id="glm-4",
-            endpoints=endpoints,
-            max_output_tokens=8192,
+            endpoints=endpoints_20_rpm,
+            max_output_tokens=4095,
         ),
         "glm-4-0520": ModelSetting(
             id="glm-4-0520",
-            endpoints=endpoints,
-            max_output_tokens=8192,
+            endpoints=endpoints_20_rpm,
+            max_output_tokens=4095,
         ),
         "glm-4-air": ModelSetting(
             id="glm-4-air",
-            endpoints=endpoints,
-            max_output_tokens=8192,
+            endpoints=endpoints_100_rpm,
+            max_output_tokens=4095,
         ),
         "glm-4-airx": ModelSetting(
             id="glm-4-airx",
-            endpoints=endpoints,
-            max_output_tokens=8192,
+            endpoints=endpoints_20_rpm,
+            max_output_tokens=4095,
         ),
         "glm-4-flash": ModelSetting(
             id="glm-4-flash",
-            endpoints=endpoints,
-            max_output_tokens=8192,
+            endpoints=endpoints_100_rpm,
+            max_output_tokens=4095,
+        ),
+        "chatglm_turbo": ModelSetting(
+            id="glm-3-turbo",
+            endpoints=endpoints_20_rpm,
+            max_output_tokens=4095,
         ),
     }
