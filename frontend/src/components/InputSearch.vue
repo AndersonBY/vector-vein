6d7723f7bf49da88a7064d85872164e77622b927
Anderson <andersonby@163.com>
2024-08-21 17:20:38 +0800

    Implement auto-clear for empty search input

--- frontend/src/components/InputSearch.vue
+++ frontend/src/components/InputSearch.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { CloseOne, Search } from '@icon-park/vue-next'
 
@@ -14,6 +15,12 @@ const clearSearch = () => {
   searchText.value = ''
   emit('clear-search')
 }
+
+watch(searchText, (newValue) => {
+  if (newValue === '') {
+    clearSearch()
+  }
+})
 </script>
 
 <template>
