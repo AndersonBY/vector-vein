dc1309621404a82ddd9d5130cdc555b68275b3b8
Anderson <andersonby@163.com>
2024-09-08 13:45:11 +0800

    Add audio download functionality to player component

--- frontend/src/components/workspace/AudioPlayer.vue
+++ frontend/src/components/workspace/AudioPlayer.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, watch } from 'vue'
-import { PlayOne, Pause } from '@icon-park/vue-next'
+import { PlayOne, Pause, Download } from '@icon-park/vue-next'
 import VueAudioPlayer from '@liripeng/vue-audio-player'
 import axios from 'axios'
 
@@ -27,7 +27,7 @@ const stop = () => {
 }
 const play = async () => {
   playing.value = true
-  const { data: song } = await axios.get(props.audios[0], {
+  const { data: song } = await axios.get(innerAudios.value[0], {
     responseType: 'arraybuffer'
   })
   player.loadArrayBuffer(song)
@@ -61,18 +61,86 @@ watch(() => props.isMidi, (newValue) => {
     loadMidiPlayer()
   }
 })
+
+const innerAudios = ref(props.audios)
+watch(() => props.audios, (newValue) => {
+  if (typeof newValue === 'string') {
+    innerAudios.value = [newValue]
+  } else {
+    innerAudios.value = newValue
+  }
+})
+
+async function downloadAudio() {
+  const currentPlayIndex = audioPlayerRef.value?.currentPlayIndex || 0
+  try {
+    const audioUrl = innerAudios.value[currentPlayIndex]
+    const response = await axios.get(audioUrl, { responseType: 'blob' })
+    const blob = new Blob([response.data])
+
+    // 从URL中提取文件名和扩展名
+    const urlObj = new URL(audioUrl)
+    const pathName = decodeURIComponent(urlObj.pathname)
+    const fileNameWithExt = pathName.split('/').pop()
+
+    // 分离文件名和扩展名
+    const lastDotIndex = fileNameWithExt.lastIndexOf('.')
+    let fileName = fileNameWithExt
+    let fileExt = ''
+
+    if (lastDotIndex !== -1) {
+      fileName = fileNameWithExt.substring(0, lastDotIndex)
+      fileExt = fileNameWithExt.substring(lastDotIndex)
+    }
+
+    // 如果没有扩展名，则根据MIME类型添加
+    if (!fileExt) {
+      const mimeType = response.headers['content-type']
+      fileExt = mimeType === 'audio/midi' || mimeType === 'audio/x-midi'
+        ? '.midi'
+        : mimeType === 'audio/mpeg'
+          ? '.mp3'
+          : mimeType === 'audio/wav'
+            ? '.wav'
+            : '.audio'
+    }
+
+    const fullFileName = `${fileName}${fileExt}`
+
+    const url = window.URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', fullFileName)
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
+    window.URL.revokeObjectURL(url)
+  } catch (error) {
+    console.error('下载音频文件时出错:', error)
+  }
+}
+
+const audioPlayerRef = ref()
 </script>
 
 <template>
-  <VueAudioPlayer :audio-list="audios" theme-color="#28c5e5" :isLoop="false" v-if="!isMidi" />
-  <div v-if="isMidi">
-    <a-button type="primary" shape="round" size="large" @click="stop" v-if="playing">
-      <pause theme="filled" />
-    </a-button>
-    <a-button type="primary" shape="round" size="large" @click="play" v-else>
+  <a-flex vertical>
+    <VueAudioPlayer v-if="!isMidi" ref="audioPlayerRef" :audio-list="innerAudios" theme-color="#28c5e5"
+      :isLoop="false" />
+    <div v-else>
+      <a-button v-if="playing" type="primary" shape="round" size="large" @click="stop">
+        <pause theme="filled" />
+      </a-button>
+      <a-button v-else type="primary" shape="round" size="large" @click="play">
+        <template #icon>
+          <play-one theme="filled" />
+        </template>
+      </a-button>
+    </div>
+    <a-button type="text" @click="downloadAudio">
       <template #icon>
-        <play-one theme="filled" />
+        <Download />
       </template>
     </a-button>
-  </div>
+  </a-flex>
 </template>
