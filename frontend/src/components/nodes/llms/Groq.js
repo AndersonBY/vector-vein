506a880608760b14312595a972d94fefcb4a23b6
Anderson <andersonby@163.com>
2024-09-19 13:25:01 +0800

    Update LLM model options for better versatility selection

--- frontend/src/components/nodes/llms/Groq.js
+++ frontend/src/components/nodes/llms/Groq.js
@@ -25,23 +25,43 @@ export function createTemplateData() {
         "required": false,
         "placeholder": "",
         "show": false,
-        "value": "groq-mixtral-8x7b-32768",
+        "value": "llama-3.1-70b-versatile",
         "options": [
           {
             "label": "mixtral-8x7b-32768",
-            "value": "groq-mixtral-8x7b-32768",
+            "value": "mixtral-8x7b-32768",
           },
           {
             "label": "llama3-70b-8192",
-            "value": "groq-llama3-70b-8192",
+            "value": "llama3-70b-8192",
           },
           {
             "label": "llama3-8b-8192",
-            "value": "groq-llama3-8b-8192",
+            "value": "llama3-8b-8192",
           },
           {
             "label": "gemma-7b-it",
-            "value": "groq-gemma-7b-it",
+            "value": "gemma-7b-it",
+          },
+          {
+            "label": "gemma2-9b-it",
+            "value": "gemma2-9b-it",
+          },
+          {
+            "label": "llama3-groq-70b-8192-tool-use-preview",
+            "value": "llama3-groq-70b-8192-tool-use-preview",
+          },
+          {
+            "label": "llama3-groq-8b-8192-tool-use-preview",
+            "value": "llama3-groq-8b-8192-tool-use-preview",
+          },
+          {
+            "label": "llama-3.1-70b-versatile",
+            "value": "llama-3.1-70b-versatile",
+          },
+          {
+            "label": "llama-3.1-8b-versatile",
+            "value": "llama-3.1-8b-versatile",
           },
         ],
         "name": "llm_model",
